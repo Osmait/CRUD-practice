7ae35d6afca31c0ee3e725331529a45ba144dfdc
Saul Burgos <saulburgos6@gmail.com>
2023-07-05 13:07:48 -0400

    add crud in memory express

--- TypeScript/express/index.ts
+++ TypeScript/express/index.ts
@@ -3,9 +3,35 @@ import { Request, Response } from "express";
 
 const app = Express();
 
+app.use(Express.json())
+
+
+interface post {
+  id: number
+  title: string,
+  conntent: string
+}
+
+let db: post[] = []
+
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("hello word ");
+  res.status(200).json(db)
 });
+
+app.post("/", (req: Request, res: Response) => {
+  const body: post = req.body;
+
+  db.push(body);
+  res.status(201).send("Created");
+})
+
+app.delete("/:id", (req: Request, res: Response) => {
+  const { id } = req.params
+  db = db.filter((p: post) => p.id != Number(id))
+  res.status(200).json(db)
+
+})
 
 app.listen(8000, () => {
   console.log("server up");
